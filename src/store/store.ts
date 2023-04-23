d1f111140d50332c789fa09e9fbaaa2005fa6428
YanPavlukovich <filin022@gmail.com>
2023-04-23 18:28:01 +0300

    создал стор, добавили наш слайс в него, пока только билеты, еще нужно потом реализовать фильтр и сортировку

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,2 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import TicketsReducer from './slices/tickets-slice';
+
+export const store = configureStore({
+	reducer: {
+		tickets: TicketsReducer,
+	}
+})
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
