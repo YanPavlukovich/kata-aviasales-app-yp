28bf2a9825f10d662515d72cc3de92dc7ceaf110
YanPavlukovich <filin022@gmail.com>
2023-04-23 18:16:52 +0300

    добавил функцию getAllTickets для рекурсивного получения всех билетов для определенного searchId

--- src/API/get-tickets.ts
+++ src/API/get-tickets.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { BASE_URL } from './core';
+import { TicketType } from '../types/tickets';
 
 
 export const getTickets = async (searchId: string) => {
@@ -9,4 +10,26 @@ export const getTickets = async (searchId: string) => {
 	} catch (e) {
 		return false;
 	}
+}
+
+let countErrors = 0;
+
+export async function getAllTickets(searchId: string): Promise<TicketType[] | undefined> {
+	try {
+		const response = await getTickets(searchId);
+		if (!response) {
+			throw new Error('Could not get all tickets')
+		}
+		const {data,status} = response;
+		return (status === 200 && !data.stop)
+			? [...data.tickets, ...(await getAllTickets(searchId) || [])]
+			: data.tickets;
+	} catch (e) {
+		countErrors++;
+		if(countErrors === 5) {
+			throw e;
+		}
+	}
+
+	return await getAllTickets(searchId);
 }
