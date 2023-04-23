7e67f7010106432458be0b78324bb90113312f4c
YanPavlukovich <filin022@gmail.com>
2023-04-23 18:34:13 +0300

    создал элемент hooks для двух хуков и их использования в компонентах

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,1 +1,6 @@
-export {};
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import type { RootState, AppDispatch } from '../store/store';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
