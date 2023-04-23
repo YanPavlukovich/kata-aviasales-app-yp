c1e7984fdbc2d4b27322d41a89085a6bb3a6e3e7
YanPavlukovich <filin022@gmail.com>
2023-04-23 18:42:15 +0300

    декомпозиция App, создал отдельный компонент HeaderIcon, декомпозиция стилей и использоавние модулей

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,11 @@
+import HeaderIcon from '../header-icon/HeaderIcon';
 import s from './App.module.scss';
-import aviaLogo from '..//header-icon/header-icon.svg';
 
 function App() {
   return (
     <div className={s.App}>
-      <header className={s['app-header']}>
-        <img src={aviaLogo} alt="Логотип компании AviaSales" />
-      </header>
-      <aside></aside>
+      <HeaderIcon />
+      <aside>Site Content</aside>
     </div>
   );
 }
--- src/components/header-icon/HeaderIcon.tsx
+++ src/components/header-icon/HeaderIcon.tsx
@@ -1,1 +1,12 @@
-export {};
+import aviaLogo from './header-icon.svg';
+import s from './/HeaderIcon.module.scss';
+
+const HeaderIcon = () => {
+  return (
+    <div className={s['app-header']}>
+      <img src={aviaLogo} alt="Логотип компании AviaSales" />
+    </div>
+  );
+};
+
+export default HeaderIcon;
