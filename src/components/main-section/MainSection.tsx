e35661ff7bdd8d77394cddd874c31a59858af1b9
YanPavlukovich <filin022@gmail.com>
2023-04-24 16:41:16 +0300

    установил андт, начинаю рисовать дерево, визуально представлять как это будет выглядеть

---
+++ src/components/main-section/MainSection.tsx
@@ -0,0 +1,15 @@
+import { Button, Space } from 'antd';
+
+const MainSection = () => {
+  return (
+    <div>
+      <div>Tabs</div>
+      <div>TicketsElements</div>
+      <Space wrap>
+        <Button type="primary">Primary Button</Button>
+      </Space>
+    </div>
+  );
+};
+
+export default MainSection;
