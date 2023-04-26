960fedf17e5f3df0724d4cee41cb596b81c39b14
YanPavlukovich <filin022@gmail.com>
2023-04-26 22:13:29 +0300

    запустил рендер по 5 билетов с сервера связал компонент Ticket со стором и с MainSection, прописал все стостояния и эффекты

--- src/components/main-section/MainSection.tsx
+++ src/components/main-section/MainSection.tsx
@@ -1,6 +1,44 @@
 import { Button, Radio, Space } from 'antd';
+import { ReactNode, useState, useEffect } from 'react';
+import { useTickets } from '../../hooks/use-tickets';
+import { useAppDispatch } from '../../hooks/hooks';
+import Ticket from '../ticket/Ticket';
+import { searchInitiated } from '../../API/search-initiate';
+import { fetchTickets, setSearchId } from '../../store/slices/tickets-slice';
 
 const MainSection = () => {
+  const [loading, setLoading] = useState(true);
+  const [maxVisible, setMaxVisible] = useState(5);
+  const selectedTickets = useTickets();
+  const dispatch = useAppDispatch();
+
+  const visibleTickets = selectedTickets.slice(0, maxVisible);
+  const ticketsElements: ReactNode = visibleTickets.map((el, i) => (
+    <Ticket key={`${el.carrier}_${i}`} {...el} />
+  ));
+
+  useEffect(() => {
+    searchInitiated().then(({ searchId }) => {
+      dispatch(setSearchId(searchId));
+      dispatch(fetchTickets(searchId));
+    });
+  }, []);
+
+  useEffect(() => {
+    if (selectedTickets.length) {
+      setLoading(false);
+      setMaxVisible(5);
+    }
+  }, [selectedTickets]);
+
+  if (loading) {
+    return <div>Загружаем билеты...</div>;
+  }
+
+  if (!selectedTickets.length) {
+    return <div>Мы ничего не нашли</div>;
+  }
+
   return (
     <div>
       <Radio.Group>
@@ -8,9 +46,11 @@ const MainSection = () => {
         <Radio.Button value="fast">Самый быстрый</Radio.Button>
         <Radio.Button value="optimum">Оптимальный</Radio.Button>
       </Radio.Group>
-      <div>TicketsElements</div>
+      {ticketsElements}
       <Space wrap>
-        <Button type="primary">Показать еще 5 билетов</Button>
+        <Button onClick={() => setMaxVisible(maxVisible + 5)} type="primary">
+          Показать еще 5 билетов!
+        </Button>
       </Space>
     </div>
   );
